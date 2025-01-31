4c39f3dc48e34ef79fe69866f27a80e0b6af1294
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2025-01-31 21:43:04 +0530

    Create Whileloop.java

---
+++ Whileloop.java
@@ -0,0 +1,15 @@
+public class Whileloop
+ {
+   public static void main(String[] args) 
+   {
+     int i =0;
+     while(i<=9)
+       {
+         System.out.println(i);
+         i++;
+         
+   }
+   }
+
+    
+
